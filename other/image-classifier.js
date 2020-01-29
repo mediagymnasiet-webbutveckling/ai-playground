9a3cd7750f1af28232d9417b943c2f152563b3d1
Sandra Larsson <sandral@mediagymnasiet.se>
2020-01-29 22:49:35 +0100

    added files

--- other/image-classifier.js
+++ other/image-classifier.js
@@ -10,7 +10,7 @@ let img;
 
 function preload() {
   classifier = ml5.imageClassifier('MobileNet');
-  img = loadImage('images/bird.png');
+  img = loadImage('images/bird.jpg');
 }
 
 function setup() {
